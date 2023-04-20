510a5ddc9aa49a93c480bbff0b539b39992b7678
YousefBe <joebelal7@gmail.com>
2023-04-21 01:30:51 +0200

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,40 @@
+import FeaturedProducts from './products/FeaturedProducts'
+import Navbar from './globals/Navbar'
+import CartButtons from './cart/CartButtons'
+import Footer from './globals/Footer'
+import Sidebar from './globals/Sidebar'
+import Hero from './globals/Hero'
+import Services from './globals/Services'
+import Contact from './globals/Contact'
+import Loading from './globals/Loading'
+import Error from './globals/Error'
+import ProductImages from './products/ProductImages'
+import AddToCart from './cart/AddToCart'
+import Filters from './globals/Filters'
+import ProductList from './products/ProductList'
+import Sort from './globals/Sort'
+import Stars from './globals/Stars'
+import CartContent from './cart/CarContent'
+import PageHero from './globals/PageHero'
+import StripeCheckout from './StripeCheckout'
+export {
+  FeaturedProducts,
+  Navbar,
+  CartButtons,
+  Footer,
+  Sidebar,
+  Hero,
+  Services,
+  Contact,
+  Loading,
+  Error,
+  ProductImages,
+  AddToCart,
+  Filters,
+  ProductList,
+  Sort,
+  Stars,
+  CartContent,
+  PageHero,
+  StripeCheckout,
+}
